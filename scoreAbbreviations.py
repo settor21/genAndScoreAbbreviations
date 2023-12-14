c805f55ecc27270f565e35b8bd6e31d55c8b54c1
settor21 <43499539+settor21@users.noreply.github.com>
2023-12-14 23:23:20 +0000

    score abbreviations comment

--- scoreAbbreviations.py
+++ scoreAbbreviations.py
@@ -1,53 +1,82 @@
-from abbreviationRules import *
-import itertools
+from abbreviationRules import *  # text formatting functions used
+import itertools  # use product method to generate all possible combinations of self-repeated abbreviations
 
 
 def scoreEachAbbreviation(gradable, markingScheme):
-    scores = {}
-    # loop through each gradable abbreviation key-value pair
+    """
+    Scores each abbreviation for a given word based on the 
+    specified rules and score card.
+
+    Args:
+    gradable (dict): A dictionary containing words as keys
+    and their non-duplicate abbreviations as values.
+    markingScheme (dict): A dictionary containing distinct letter scores 
+    from values.txt.
+
+    Returns:
+    dict: A dictionary containing word scores with their corresponding abbreviations.
+    """
+    scores = {}  # hold scores for this text batch
+    # loop through each gradable abbreviation key-value pair when sorted
     for word, abbreviations in sorted(gradable.items()):
-        wordScores = {}  # dictionary to hold scores
-        for abbreviation in abbreviations:
+        wordScores = {}  # dictionary to hold all word scores
+        for abbreviation in abbreviations:  # for each abbrev
             cleanedText = cleanText(word)  # filter the original name
             dividedWord = cleanedText.split()  # used to check indexing during test
+            # identify all text positions
             indexedWord = indexedText(cleanedText)
+            # check if the abbreviation can be created from the same word more than once and has been scored already
             if abbreviation in wordScores:
                 abbreviationScore = calculateScoreForAllCombinations(
                     abbreviation, indexedWord, markingScheme, dividedWord)
                 wordScores[abbreviation] = abbreviationScore
-            else:
+            else:  # abbreviation is generated from word once
                 abbreviationScore = calculateScore(
                     abbreviation, indexedWord, markingScheme, dividedWord)
+                # assign the abbreviation and its score to wordScores
                 wordScores[abbreviation] = abbreviationScore
+        # assign the dictionary of abbreviation scores for the word to scores
         scores[word] = wordScores
     return scores
 
+# function to swap indexes of first two elements in list
 
-def swap(lst):
-    if len(lst) >= 2:
+
+def swapFirstTwo(lst):
+    if len(lst) >= 2:  # parameter check
         lst[0], lst[1] = lst[1], lst[0]
 
+
 def calculateScore(abbreviation, indexedWord, markingScheme, word):
+    """
+    Calculates the score for a given abbreviation based on the 
+    specified rules and marking scheme.
+
+    Args:
+    abbreviation (str): The abbreviation to be scored.
+    indexedWord (dict): The indexed representation of the cleaned word.
+    markingScheme (dict): The dictionary containing alphabet scores.
+    word (list): Reference for the indexedWord. Used in debugging.
+
+    Returns:
+    int: The total score for the abbreviation.
+    """
     # store each letter from abbreviation as variable
     firstLetter = abbreviation[0]
     secondLetter = abbreviation[1]
     thirdLetter = abbreviation[2]
-     
 
     # Makes it easy to score each distinct letter
     def getScore(letter):
-        """
-        For repeated instances of a letter within a word, 
-        obtain its index from the word index and shift to the next index 
-        to avoid repeated indexing of letters in abbreviations like COO. 
-        """
+        # Obtain index values for the letter and pop the first value
         index_values = indexedWord.get(letter, [3])
         index_value = index_values.pop(0) if index_values else 3
-        
-        # Swap next two positions if the condition is met
+
+        # Swap next two positions if first and third letter repeats
         if index_value == 0 and firstLetter == thirdLetter and firstLetter != secondLetter:
-            swap(index_values)
-        # specified rules
+            swapFirstTwo(index_values)
+
+        # specified rules for scoring letters
         if index_value == 0:
             return 0
         elif index_value == -1:
@@ -65,30 +94,31 @@ def calculateScore(abbreviation, indexedWord, markingScheme, word):
     firstLetterScore = getScore(firstLetter)
     secondLetterScore = getScore(secondLetter)
     thirdLetterScore = getScore(thirdLetter)
-    # print(f"{word}\n")
-    # print(f"{abbreviation}")
-    # print(firstLetterScore)
-    # print(secondLetterScore)
-    # print(thirdLetterScore)
-    # print()
     # return the sum
     return firstLetterScore + secondLetterScore + thirdLetterScore
 
-# custom function to score words with multiple abbreviations
-
 
 def calculateScoreForAllCombinations(abbreviation, indexedWord, markingScheme, word):
+    """
+    Calculates the score for all repeated combinations of a given abbreviation 
+    based on the specified rules and marking scheme. eg. ZAZ Zanzamaamzama
+
+    Args:
+    abbreviation (str): The abbreviation to be scored.
+    indexedWord (dict): The indexed representation of the cleaned word.
+    markingScheme (dict): The dictionary containing alphabet scores.
+    word (list): Reference for the indexedWord. Used in debugging.
+
+    Returns:
+    int: The total score for the abbreviation.
+    """
     firstLetter = abbreviation[0]
     secondLetter = abbreviation[1]
     thirdLetter = abbreviation[2]
     # function to determine a letter score using the specified rules
 
     def getAllScores(letter):
-        """
-        For repeated instances of a letter within a word, 
-        obtain its index from the word index and shift to the next index 
-        to avoid repeated indexing of letters in abbreviations like COO. 
-        """
+
         index_values = indexedWord.get(letter, [3])
         index_value = index_values.pop(0) if index_values else 3
         # rules specified
@@ -105,21 +135,25 @@ def calculateScoreForAllCombinations(abbreviation, indexedWord, markingScheme, w
             return 2 + markingScheme.get(letter, 0)
         else:
             return 3 + markingScheme.get(letter, 0)
-
+        
+    # Generate all combinations of indices for each letter 
+    # using itertools.product for faster computation
     all_combinations = itertools.product(
         indexedWord[firstLetter], indexedWord[secondLetter], indexedWord[thirdLetter]
     )
 
+    # arbitrary minimum score for quick computation
     min_score = float('inf')
-
+    
+    # Loop through each combination and calculate scores
     for combination in all_combinations:
         firstLetterScore = getAllScores(firstLetter)
         secondLetterScore = getAllScores(secondLetter)
         thirdLetterScore = getAllScores(thirdLetter)
 
         total_score = firstLetterScore + secondLetterScore + thirdLetterScore
-        min_score = min(min_score, total_score)
-
+        # compare each iterations score and get the minimum
+        min_score = min(min_score, total_score) 
     return min_score
 
 
@@ -131,16 +165,19 @@ def findLowestScoreAbbreviation(scores):
         lowest_score_abbreviations = []
 
         for abbreviation, score in sorted(wordScores.items()):
+            # Check if the score is lower than the current lowest score
             if score < lowest_score:
+                # Update the lowest score
                 lowest_score = score
                 lowest_score_abbreviations = [abbreviation]
-            elif score == lowest_score:
+            elif score == lowest_score: # check for similar scores and add them
                 lowest_score_abbreviations.append(abbreviation)
-
+                
+        # Update the lowestScoredAbbreviation dictionary with the final result
         if word not in lowestScoredAbbreviation:
             lowestScoredAbbreviation[word] = {}
-
+        #join multiple abbreviations or the single abbreviation with lowest score into screen 
         lowestScoredAbbreviation[word][' '.join(
-            lowest_score_abbreviations)] = lowest_score
+            lowest_score_abbreviations)] = lowest_score  # add them to the dictionary
 
-    return lowestScoredAbbreviation
+    return lowestScoredAbbreviation #return the final dictionary
